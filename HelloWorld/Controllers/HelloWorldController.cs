c4c8f70b8e8caf8a9c358006701fd00db0bfa82c
programer10 <programer10@ucione.local>
2019-03-29 17:31:22 +0100

    dodan 4.1.1 zadatak

---
+++ HelloWorld/Controllers/HelloWorldController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult PopisKosarice()
+        {
+            return View();
+        }
+        public ActionResult ListaArtikala()
+        {
+            string[] lista = new string[] { "Skije", "Pancerice", "Sunčane naočale", "Šal", "Kapa" };
+            ViewBag.Lista = lista;
+            return View();
+        }
+
+        public ActionResult RedirectNaMetodu(string id)
+        {
+            if (id=="Kosarica")
+            {
+                return RedirectToAction("PopisKosarice");
+            }
+            else
+            {
+                return RedirectToAction("ListaArtikala");
+            }
+        }
+    }
+}
