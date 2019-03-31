5b3ec2fafb6842697bac5a6df8e3b119f8360a39
Davor85 <davor.lovric85@gmail.com>
2019-03-31 13:56:54 +0200

    dodana zadaca

---
+++ 4.1.1_broj_godina/Controllers/TocanOdgovorController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _4._1._1_broj_godina.Controllers
+{
+    public class TocanOdgovorController : Controller
+    {
+        
+        public ViewResult ProvjeriOdgovor()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult ProvjeriOdgovor(string odgovor)
+        {
+            string rezultat;
+            if (!string.IsNullOrEmpty(odgovor))
+            {
+                if (odgovor =="Bruxelles")
+                {
+                    rezultat = "Odgovor je točan!";
+                    return View((object)rezultat);
+                }
+                else
+                {
+                    rezultat = "Odgovor nije točan";
+                    return View((object)rezultat);
+                }
+            }
+            else
+            {
+                rezultat = "Odgovor ne postoji!";
+                return View((object)rezultat);
+            }
+        }
+
+    }
+}
---
+++ 4.2.1_narudzba/Controllers/NarudzbaArtiklaController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _4._2._1_narudzba.Controllers
+{
+    public class NarudzbaArtiklaController : Controller
+    {
+        
+        public ViewResult NaruciArtikal()
+        {
+            return View(new Artikal());
+        }
+        [HttpPost]
+        public ViewResult NaruciArtikal(Artikal artikal)
+        {
+            if (artikal.Kolicina > 10)
+            {
+                ViewBag.Poruka = "Nema dovoljno" + artikal.Naziv + "na skladištu!";
+                return View(artikal);
+            }
+            else
+            {
+                ViewBag.Poruka = "Naruceno je " +
+                    artikal.Kolicina +
+                    "komada" +
+                    artikal.Naziv +
+                    "sa ukupnom cijenom" +
+                    artikal.Cijena * artikal.Kolicina;
+                return View(artikal);
+            }
+        }
+    }
+}
