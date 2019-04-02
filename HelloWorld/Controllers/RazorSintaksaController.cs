3a9b2cf9ebde7c0812b5c3ee7be085ff3fcd1353
programer10 <programer10@ucione.local>
2019-04-02 16:26:28 +0200

    primjeri razors petlji

---
+++ HelloWorld/Controllers/RazorSintaksaController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class RazorSintaksaController : Controller
+    {
+        // GET: RazorSintaksa
+        public ViewResult SwitchView()
+        {
+            return View();
+        }
+
+        public ViewResult ViewZaForPetlju()
+        {
+            string[] voce = new string[]
+            {
+                "Jabuka",
+                "Kruska",
+                "Banana",
+                "Grozdje",
+                "Sljiva"
+            };
+            return View(voce);
+        }
+    }
+}
