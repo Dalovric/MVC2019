197ec3549a62e7d68fbf3e28e8f61b9cf999bbc5
programer10 <programer10@ucione.local>
2019-04-04 17:13:53 +0200

    zad 6.2 iz prirucnika

---
+++ zad6_2/Controllers/TempHtmlHelperiController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using zad6_2.Models;
+
+namespace zad6_2.Controllers
+{
+    public class TempHtmlHelperiController : Controller
+    {
+        // GET: TempHtmlHelperi
+        public ViewResult HtmlEditorView()
+        {
+            return View(new OsobaTemp1());
+        }
+        [HttpPost]
+        public ViewResult HtmlEditorView(OsobaTemp1 osoba)
+        {
+            return View("HtmlLabelDisplay", osoba);
+        }
+        public ViewResult EditorModelView()
+        {
+            return View(new OsobaTemp1());
+        }
+        [HttpPost]
+        public ViewResult EditorModelView(OsobaTemp1 osoba)
+        {
+            return View("HtmlLabelDisplay", osoba);
+        }
+    }
+}
---
+++ zad6_2/Models/Adresa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace zad6_2.Models
+{
+    public class Adresa
+    {
+        public string Ulica { get; set; }
+        public string KucniBroj { get; set; }
+        public int Kat {get; set;}
+    }
+
+}
