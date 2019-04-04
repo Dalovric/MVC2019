db399a40353642678f51f7eb0913b66ce7eada54
programer10 <programer10@ucione.local>
2019-04-04 18:40:48 +0200

    dodani meta data

---
+++ zad6_2/Controllers/MetaDataHtmlHelperController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using zad6_2.Models;
+
+namespace zad6_2.Controllers
+{
+    public class MetaDataHtmlHelperController : Controller
+    {
+        // GET: MetaDataHtmlHelper
+        public ViewResult MetaDataView()
+        {
+            return View(new OsobaMeta());
+        }
+        [HttpPost]
+        public ViewResult MetaDataView(OsobaMeta osoba)
+        {
+            return View("/HtmlLabelDisplay", osoba);
+        }
+        
+    }
+}
