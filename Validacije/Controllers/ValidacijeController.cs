52d339a2e29655670e00e4daaf76376fcf1857c3
programer10 <programer10@ucione.local>
2019-04-05 17:21:16 +0200

    Dodane error validacije

---
+++ Validacije/Controllers/ValidacijeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Validacije.Models;
+
+namespace Validacije.Controllers
+{
+    public class ValidacijeController : Controller
+    {
+        // GET: Validacije
+        public ViewResult IzdavanjaRacuna()
+        {
+            return View(new Racun() { Datum = DateTime.Now, BrojRacuna = "/" + DateTime.Now.Year.ToString() });
+        }
+        [HttpPost]
+        public ViewResult IzdavanjeRacuna(Racun racun)
+        {
+            // obavezne vrijednosti
+            if (string.IsNullOrEmpty(racun.BrojRacuna))
+            {
+                ModelState.AddModelError("BrojRacuna", "Broj računa je obavezan!");
+            }
+            if (string.IsNullOrEmpty(racun.Zaposlenik))
+            {
+                ModelState.AddModelError("Zaposlenik", "Zaposlenik je obavezan!");
+            }
+            if (string.IsNullOrEmpty(racun.Kupac))
+            {
+                ModelState.AddModelError("Kupac", "Kupac je obavezan!");
+            }
+            // model-level validacija
+            if (ModelState.IsValidField("Datum"))
+            {
+                if (racun.Datum < DateTime.Today.AddDays(-3))
+                {
+                    ModelState.AddModelError("", "Datum ne smije biti manji za vise od 3 dana");
+                }
+            }
+            //ukupna promjena validacije
+            if (ModelState.IsValid)
+            {
+                return View("RacunIzdan", racun);
+            }
+            else
+            {
+                return View();
+            }
+
+            
+        }
+        public ViewResult IzdavanjaRacuna2()
+        {
+            return View(new Racun() { Datum = DateTime.Now, BrojRacuna = "/" + DateTime.Now.Year.ToString() });
+        }
+    }
+}
