a46b8b2591ff4a95db14fbbd2714b0891cea91e7
programer10 <programer10@ucione.local>
2019-04-08 18:04:16 +0200

    spajanje na bazu

---
+++ Adonet_spajanje_na_bazu1/Controllers/SqlConnectionObjectController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Adonet_spajanje_na_bazu1.Controllers
+{
+    public class SqlConnectionObjectController : Controller
+    {
+        // GET: SqlConnectionObject
+        public ActionResult Index()
+        {
+            // Prvo kreiramo conn string
+            string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=dbAlgebra;Integrated Security=True";
+
+            // Nakon toga instanca SQL connection
+            SqlConnection conn = new SqlConnection(connString);
+
+            try
+            {
+                conn.Open();
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    Response.Write("Konekcija je uspjela");
+                }
+            }
+
+            catch (SqlException sqlEx)
+            {
+                Response.Write(sqlEx.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return View();
+        }
+        // GET: SqlConnectionObject
+        public ActionResult SpojPrekoWebConfig()
+        {
+            // Prvo kreiramo conn string
+            //string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=dbAlgebra;Integrated Security=True";
+            string connString = ConfigurationManager.ConnectionStrings["dbAlgebraConnString"].ConnectionString;
+            // Nakon toga instanca SQL connection
+            SqlConnection conn = new SqlConnection(connString);
+
+            try
+            {
+                conn.Open();
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    Response.Write("Konekcija je uspjela");
+                }
+            }
+
+            catch (SqlException sqlEx)
+            {
+                Response.Write(sqlEx.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Adonet_spajanje_na_bazu1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Adonet_spajanje_na_bazu1.MvcApplication" Language="C#" %>
