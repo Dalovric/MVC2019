1ce259809aedd1ca285f7fdd88dadcc43efacd4b
programer10 <programer10@ucione.local>
2019-04-12 17:24:31 +0200

    8.4 zbirka generirani model

---
+++ 8_4_WebShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_8_4_WebShop.Startup))]
+namespace _8_4_WebShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
