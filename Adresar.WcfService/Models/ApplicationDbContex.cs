b18727545b025fdbc82b5ce9cb5ac76823823e1d
programer10 <programer10@ucione.local>
2019-04-17 17:33:17 +0200

    Adresar wcfservice

---
+++ Adresar.WcfService/Models/ApplicationDbContex.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Adresar.WcfService.Models
+{
+    public class ApplicationDbContex: DbContext
+    {
+        public ApplicationDbContex()
+            : base("AdresarBaza")
+        {
+
+        }
+        public DbSet<Kontakt> Kontakti { get; set; }
+    }
+}
